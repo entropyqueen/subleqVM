056477fb811426ec51c64e8728a08116a7c0c379
emy <emy.rg@protonmail.com>
2019-04-02 13:54:33 +0200

    vm.py flake8 compliant

--- vm.py
+++ vm.py
@@ -7,6 +7,7 @@ import pickle
 from termcolor import colored
 from time import sleep
 
+
 class VM:
 
     DISPLAY_SPACING = 10
@@ -19,9 +20,9 @@ class VM:
         self.debug = False
         self.verbose = verbose
 
-    ##
-    ## Debugging utilities
-    ##
+    #
+    # Debugging utilities
+    #
 
     def fmt(self, r):
         i, r = r
@@ -49,11 +50,13 @@ class VM:
 
     def dump_init(self):
         if self.verbose:
-            print("".join("%-*d" % (self.DISPLAY_SPACING, i) for i in range(self.size)))
+            print("".join("%-*d" % (
+                self.DISPLAY_SPACING, i
+                ) for i in range(self.size)))
 
-    ##
-    ## Program loader utilities
-    ##
+    #
+    # Program loader utilities
+    #
 
     def prog_parse_from_file(self, f_name):
         with open(f_name, 'rb') as f:
@@ -64,13 +67,14 @@ class VM:
         self.mem = [random.randrange(0, vm.size**3) for _ in range(self.size)]
 
     def load(self, mem):
-        assert len(mem) <= self.size, "Memory error (VM does not have enough memory)."
+        assert len(mem) <= self.size, \
+            "Memory error (VM does not have enough memory)."
         for i, d in enumerate(mem):
             self.mem[i] = d
 
-    ##
-    ## Implementation
-    ##
+    #
+    # Implementation
+    #
 
     def decode(self):
         b, c = divmod(self.mem[self.pc], self.size)
@@ -81,7 +85,8 @@ class VM:
 
     def subleq(self):
         a, b, c = self.decode()
-        assert -self.size < a < self.size and -self.size < b < self.size, "Segmentation fault."
+        assert -self.size < a < self.size and -self.size < b < self.size, \
+            "Segmentation fault."
 
         aa = self.mem[a]
         bb = self.mem[b]
@@ -97,24 +102,26 @@ class VM:
         if self.speed:
             sleep(self.speed)
 
+
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='OISC VM implementation using subleq instructions.')
+    parser = argparse.ArgumentParser(
+            description='OISC VM implementation using subleq instructions.')
     grp = parser.add_mutually_exclusive_group(required=True)
     grp.add_argument('file', metavar='FILE', nargs='?', default=None,
-            help='bytecode to load (compiled with asm.py)')
+                     help='bytecode to load (compiled with asm.py)')
     grp.add_argument('--seed', '-s', metavar='SEED', default=None,
-            help='seed for loading random program')
+                     help='seed for loading random program')
     parser.add_argument('--memsz', '-m', metavar='SIZE', default=16, type=int,
-            help='change the virtual memory size (default is 16)')
-    parser.add_argument('--verbose', '-v', action='store_const', default=False, const=True,
-            help='be verbose')
+                        help='change the virtual memory size (default is 16)')
+    parser.add_argument('--verbose', '-v', action='store_const', default=False,
+                        const=True, help='be verbose')
     args = parser.parse_args()
 
     vm = VM(args.memsz, speed=0.1, verbose=args.verbose)
 
     # Load a program (either from file or random)
-    if args.file != None:
+    if args.file is not None:
         prog = vm.prog_parse_from_file(args.file)
         try:
             vm.load(prog)
